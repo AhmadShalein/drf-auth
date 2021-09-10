dbf42ee3f070cf82143a92a7aa41ef3fb2a16ab1
AhmadShalein <ahmadshalein@gmail.com>
2021-09-11 00:35:06 +0300

    Lab 33

--- post/models.py
+++ post/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=64)
     body = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,3 +11,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post.detail',args=(self.id) ,kwargs={'pk': self.pk})
